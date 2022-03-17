0bb71e33d390c4d348cb8e398fad23d059a94e60
Alec Bills <abills@andrew.cmu.edu>
2022-03-17 17:56:03 -0400

    Terminating callback works

--- src/events.jl
+++ src/events.jl
@@ -3,26 +3,44 @@
 #
 using DiffEqBase
 
-function build_callback(event)
+function build_callback(event,size)
     type = event.event_type.value
     scalar_check = event.expression.__class__.__name__
     if scalar_check=="Scalar"
         return nothing
     end
     if type==0
-        jl_event = build_terminating_callback(event)
+        jl_event = build_terminating_callback(event,size)
     elseif type==1
         jl_event = build_discontinuity_callback(event)
     elseif type==2
         jl_event = build_interpolant_extrapolation_callback(event)
     elseif type==3
-        jl_event = build_switch_callback(event)
+        jl_event = build_terminating_callback(event,size)
     else
         error("unrecognized callback")
     end
     return jl_event
 end
 
-function build_terminating_callback(event)
-    jl_func = runtime_eval(Meta.parse(pybamm.get_julia_function(event.expression)))
+function build_terminating_callback(event,size)
+    #Generate Julia Function with the Event
+    jl_str = pybamm.get_julia_function(event.expression)
+    jl_func! = runtime_eval(Meta.parse(jl_str))
+    #Generate Condition for Callback
+    
+    
+    
+    f = begin
+    f2 = let cs = (
+        cache_du = zeros(size),
+    )
+    function f3(u,p,t)
+    jl_func!(cs.cache_du,u,p,t)
+    return cs.cache_du[1]
+    end
+    end
+    end
+    callback = ContinuousCallback(f,terminate!)
+    return callback
 end
