983a8cb2ccf00af31a9b122505d5ae7543648a7a
Alexander Bills <abills@andrew.cmu.edu>
2022-11-07 12:54:09 -0500

    finish termination conditions

--- src/experiments.jl
+++ src/experiments.jl
@@ -29,14 +29,58 @@ function get_termination_condition(sim, inputs, tend)
     #I'm just going to do this myself.
     op_conds = sim.experiment.operating_conditions
     num_steps = length(op_conds)
+    exprs = []
+    event_inputs = []
+
     funcs = []
     for step in 1:num_steps
+        exprs = []
+        sv = pybamm.StateVector(pyslice(0,1))
         op_cond = op_conds[step-1]
-        #Check for Time First
-        for event in op_cond["events"]
+        
+        #Check for Time First.
+        max_t = pybamm.Scalar(op_cond["time"])
+        t = pybamm.Time()
+        pybamm_time_expr = max_t - t
 
+        push!(exprs, pybamm_time_expr)
+        push!(event_inputs, t)
 
+        #Check for Current Second.
+        if "Current cut-off [A]" in op_cond
+            current = pybamm.AbsoluteValue(sim.op_conds_to_built_models[op_cond["string"]].variables["Current [A]"])
+            max_current = pybamm.Scalar(op_cond["Current cut-off [A]"])
+            pybamm_current_expr = current - max_current
+            push!(exprs, pybamm_current_expr)
+        end
 
+        #Check for Voltage Third.
+        if "Voltage cut-off [V]" in op_cond
+            voltage = sim.op_conds_to_built_models[op_cond["string"]].variables["Terminal voltage [V]"]
+            if pyconvert(String, op_cond["type"]) == "power"
+                inp = pyconvert(Float64, op_cond["Power input [W]"])
+            elseif pyconvert(String, op_cond["type"]) == "current"
+                inp = pyconvert(Float64, op_cond["Current input [A]"])
+            else
+                print(pyconvert(String, op_cond["type"]))
+                error("Unknown type")
+            end
+
+            sign_jl = sign(inp)
+            sign_py = pybamm.Scalar(sign_jl)
+
+            voltage_expr = sign_py * (voltage - pybamm.Scalar(op_cond["Voltage cut-off [V]"]))
+            push!(exprs, voltage_expr)
+        end
+        out_expr = pybamm.numpy_concatenation(exprs...)
+        out_expr = pybamm.PybammJuliaFunction([sv, t], out_expr, "check_for_termination", false)
+        
+        func_converter = pybamm.JuliaConverter()
+        func_converter.convert_tree_to_intermediate(out_expr)
+        jl_str = pyconvert(String, func_converter.build_julia_code())
+        jl_func = eval(Meta.parse(jl_str))
+        push!(funcs, jl_func)
+    end
     return funcs
 end
 
@@ -60,7 +104,7 @@ function set_initial_conditions!(new_u, old_model, new_model, last_u)
         start_index = pyconvert(Int, slice.start) + 1
         stop_index = pyconvert(Int, slice.stop)
         sl_step = pyconvert(Any, slice.step)
-        if sl_step == nothing
+        if sl_step === nothing
             new_u[start_index:stop_index] .= Base.@invokelatest jl_func(last_u, 0)
         else
             step = pyconvert(Int, slice.step)
